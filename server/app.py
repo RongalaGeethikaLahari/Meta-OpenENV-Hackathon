867917057b2210a1c5380a79c228709b128e5798
AdamKoda2306 <adam.koda2306@gmail.com>
2026-04-08 11:37:05 +0530

    fixed scripts

--- server/app.py
+++ server/app.py
@@ -2,8 +2,23 @@ from openenv.core.env_server import create_fastapi_app
 from server.environment import EmailEnv
 from core.models import EmailAction, EmailObservation
 
+import uvicorn
+
 app = create_fastapi_app(
     EmailEnv,
     EmailAction,
     EmailObservation
 )
+
+# ✅ REQUIRED ENTRY POINT
+def main():
+    uvicorn.run(
+        "server.app:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=False
+    )
+
+# ✅ REQUIRED
+if __name__ == "__main__":
+    main()
